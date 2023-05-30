002db73ab070fc7a309f35f75bd54f0b13627600
MicheleIlPro <126272800+MicheleIlPro@users.noreply.github.com>
2023-05-30 18:33:22 +0200

    aggiunto il router con le pagine collegate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './mains/home/home.component';
+import { BotsComponent } from './mains/bots/bots.component';
+import { ServerComponent } from './mains/server/server.component';
+import { NullaComponent } from './mains/nulla/nulla.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'bots', component: BotsComponent },
+  { path: 'server', component: ServerComponent },
+  { path: 'nulla', component: NullaComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
