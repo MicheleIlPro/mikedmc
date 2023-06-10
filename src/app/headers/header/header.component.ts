a9b6c2b662e21c9fcd46fe14a3ab172a4a8bae79
MicheleIlPro <126272800+MicheleIlPro@users.noreply.github.com>
2023-06-10 02:29:13 +0200

    page bots, section prezzi, aggiunto, responcive

--- src/app/headers/header/header.component.ts
+++ src/app/headers/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, ElementRef, ViewChild  } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -6,12 +7,46 @@ import { Component, ElementRef, ViewChild  } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  // @ViewChild('menu') menuRef: ElementRef | undefined;
+  // @ViewChild('nav') navRef: ElementRef | undefined;
+
+  // toggleNav() {
+  //   if (this.menuRef && this.navRef) {
+  //     this.navRef.nativeElement.classList.toggle("active");
+  //   }
+  // }
+
   @ViewChild('menu') menuRef: ElementRef | undefined;
   @ViewChild('nav') navRef: ElementRef | undefined;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        this.updateActiveClass();
+      }
+    });
+  }
 
   toggleNav() {
     if (this.menuRef && this.navRef) {
       this.navRef.nativeElement.classList.toggle("active");
     }
   }
+
+  updateActiveClass() {
+    const currentUrl = this.router.url;
+    const links = document.querySelectorAll('a');
+
+    links.forEach(link => {
+      const href = link.getAttribute('href');
+      if (href) {
+        if (href === currentUrl) {
+          link.classList.add('active');
+        } else {
+          link.classList.remove('active');
+        }
+      }
+    });
+  }
+
 }
